b0aa084d019baa2777c749f0df879f3a0dbfbfe3
Andrew Soknacki <asoknacki@gmail.com>
2015-10-09 13:05:12 -0400

    importing sys, was missed

--- sigrok_meter/qtcompat.py
+++ sigrok_meter/qtcompat.py
@@ -17,7 +17,7 @@
 ## along with this program; if not, write to the Free Software
 ## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 ##
-
+import sys
 def load_modules(force_pyside):
     if force_pyside:
         import PySide.QtCore as _QtCore
